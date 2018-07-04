7970d5b2ff674e373fba300546010f3b18c5be6a
Tomasz Wychocki <tomasz.wychocki@tieto.com>
2018-07-04 23:57:20 +0200

    Program nr 1 kwadrat

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+     int rozmiarKwadratu;
+
+     cout << "Podaj rozmiar kwadratu: ";
+     cin >> rozmiarKwadratu;
+    
+    for(int i = 1; i <= rozmiarKwadratu; i++)
+    {
+                
+        for(int j = 1; j <= rozmiarKwadratu; j++)
+        {
+            if(j <= i)
+            {
+                if(i%2)
+                {
+                    cout << 'X' ;
+                    
+                }
+                else
+                {
+                    cout << 'O';
+                }
+                
+            }
+            else
+            {
+                cout << '-';
+            }
+            
+        }
+        
+
+        cout << endl;
+        
+    }
+    
+    
+    
+
+    return 0;
+    /**
+     * x
+     * xx
+     * xxx
+     * xxxx
+     * xxxxx
+     * xxxxxx
+     */
+}
